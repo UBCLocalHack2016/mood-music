562a00a13680308e614058f12ae4e26d3372804a
Sherry Yuan <frostyyshadows@gmail.com>
2016-12-03 12:05:36 -0800

    added client oauth

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 var http    = require('http');
 var twitter = require('twitter');
 
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+
+console.log(process.env.TWITTER_CONSUMER_KEY + " " + process.env.TWITTER_CONSUMER_SECRET
+    + " " + process.env.TWITTER_ACCESS_TOKEN_KEY + " " + process.env.TWITTER_ACCESS_TOKEN_SECRET);

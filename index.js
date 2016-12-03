8e3a0bdaa070e97c60da36df54836d5082ca0367
Sherry Yuan <frostyyshadows@gmail.com>
2016-12-03 12:51:52 -0800

    not much

--- index.js
+++ index.js
@@ -7,17 +7,20 @@ var client = new Twitter({
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
+
 console.log(process.env.TWITTER_CONSUMER_KEY + " " + process.env.TWITTER_CONSUMER_SECRET + " " + process.env.TWITTER_ACCESS_TOKEN_KEY + " " + process.env.TIWTTER_ACCESS_TOKEN_SECRET);
-console.log('Consumer key: ' + client.consumer_key);
-console.log('Consumer secret: ' + client.consumer_secret);
-console.log('Access token key: ' + client.access_token_key);
-console.log('Access token secret: ' + client.access_token_secret);
+console.log(JSON.stringify(client));
+console.log('Consumer key: ' + client.options.consumer_key);
+console.log('Consumer secret: ' + client.options.consumer_secret);
+console.log('Access token key: ' + client.options.access_token_key);
+console.log('Access token secret: ' + client.options.access_token_secret);
 
 var params = {track: '#MoodMusicPls'};
 
 var stream = client.stream('statuses/filter', params);
 stream.on('data', function(event) {
     console.log(event && event.text);
+    var text = event.text;
 });
 stream.on('error', function(error) {
     console.log(error);

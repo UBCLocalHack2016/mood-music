6d6c34ed25f4df4f0f608a9305e9a70314b29dc5
rwblickhan <rwblickhan@gmail.com>
2016-12-04 17:02:27 -0800

    Begin cleanup

--- index.js
+++ index.js
@@ -1,50 +1,19 @@
-var http    = require('http');
-var https = require('https');
-var Twitter = require('twitter');
-var SpotifyWebApi = require('spotify-web-api-node');
-var request = require('request'); // "Request" library
+var http    = require('http');    //node http requests library
+var https   = require('https');   //node https requests library
+var Twitter = require('twitter'); //Twitter API wrapper library
+var request = require('request'); //authorization request library
 
-var client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
+//hashtag we will listen for
 var hashtag = "#MoodMusicPls";
-var params = {track: hashtag};
 
+//Spotify access token
+var accessToken = '';
+
+//Spotify auth details
 var client_id = process.env.SPOTIFY_CLIENT_ID;
 var client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 
-var stream = client.stream('statuses/filter', params);
-stream.on('data', function(tweet) {
-    console.log(tweet && tweet.text);
-    analyzeTweet(tweet.user.screen_name, tweet.text.replace(hashtag, ''));
-});
-stream.on('error', function(error) {
-    console.log(error);
-});
-
-
-// var authOptions={
-//     url:  'https://accounts.spotify.com/api/token',
-//     method:"POST",
-//     headers: {
-//         'Authorization': 'Basic' + (new Buffer(spotifyApi.client_id + ':' + spotifyApi.client_secret).toString('base64'))
-//     },
-//     body: "grant_type=client_credentials&scope=playlist-modify-public playlist-modify-private"
-// };
-//
-// requests(authOptions,fuction(err, res, body)){
-//     console.log('error', err);
-//     console.log('status', res.statusCode);
-//     console.log('body', body);
-// });
-// }
-
-
-
-// your application requests authorization
+//Spotify authorization request options
 var authOptions = {
   url: 'https://accounts.spotify.com/api/token',
   headers: {
@@ -56,15 +25,32 @@ var authOptions = {
   json: true
 };
 
-var accessToken = '';
-
+//request Spotify access token
 request.post(authOptions, function(error, response, body) {
   if (!error && response.statusCode === 200) {
       accessToken = body.access_token;
   }
 });
 
+//create new Twitter listener via API wrapper library
+var params = {track: hashtag};
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
+//listen for Tweets with our hashtag
+var stream = client.stream('statuses/filter', params);
+stream.on('data', (tweet) => {
+    analyzeTweet(tweet.user.screen_name, tweet.text.replace(hashtag, ''));
+});
+stream.on('error', (error) => {
+    console.log(error);
+});
+
+//send tweet to sentiment analysis API
 function analyzeTweet(username, text){
     var postData = 'txt=' + text;
     var bodyChunks = [];
@@ -100,34 +86,7 @@ function analyzeTweet(username, text){
     req.end();
 }
 
-// function authSpotify(username, confidence, sentiment) {
-//     var bodyChunks = [];
-//     var options = {
-//         hostname: 'accounts.spotify.com',
-//         path: '/api/token',
-//         method: 'POST',
-//         headers: {
-//             'Authorization': 'Basic ' + client_id + client_secret
-//         }
-//     };
-//     var req = https.request(options, (res) => {
-//         res.setEncoding('utf8');
-//         res.on('data', (chunk) => {
-//             bodyChunks.push(chunk);
-//         });
-//         res.on('end', () => {
-//             console.log('Data received: ' + bodyChunks);
-//             getRecommendation(username, confidence, sentiment, JSON.stringify(bodyChunks).access_token);
-//         });
-//         res.on('error', (err) => {
-//             console.log(err);
-//         });
-//     });
-//
-//     req.write('grant_type=client_credentials');
-//     req.end();
-// }
-
+//convert sentiment into recommendation params and get recommendation from Spotify API
 function getRecommendation(username, confidence, sentiment, accessToken) {
     var bodyChunks = [];
     var adjective = 'neutral';
@@ -151,8 +110,6 @@ function getRecommendation(username, confidence, sentiment, accessToken) {
         pathString += '&mode=' + mode;
     }
 
-    console.log('api.spotify.com' + pathString);
-
     var options = {
         hostname: 'api.spotify.com',
         path: pathString,
@@ -163,16 +120,12 @@ function getRecommendation(username, confidence, sentiment, accessToken) {
 
     var req = https.request(options, (res) => {
             res.on('data', function (chunk) {
-                console.log(chunk);
                 bodyChunks.push(chunk);
             });
             res.on('end', function (chunk) {
                 var resultObj = JSON.parse(Buffer.concat(bodyChunks).toString());
-                console.log('resultObj: ' + JSON.stringify(resultObj));
-                console.log(resultObj);
                 var randomInt = Math.floor(Math.random() * resultObj.tracks.length);
                 var url = resultObj.tracks[randomInt].external_urls.spotify;
-                console.log('Sending response');
                 sendResponse(username, adjective, url);
             });
             res.on('error', function (error) {
@@ -183,6 +136,7 @@ function getRecommendation(username, confidence, sentiment, accessToken) {
     req.end();
 }
 
+//send response tweet to user
 function sendResponse(username, desc, url) {
     var statusText = 'Hey @' + username + ', you sound ' + desc + '. Here\'s a playlist for you: ' + url;
     client.post('statuses/update', {status: statusText}, function (error, tweet, response) {
